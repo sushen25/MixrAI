53587bc3adb5a6006027a6c08035a7cf38c5d0fe
Sushen Satturu <sushensatturu25@gmail.com>
2024-01-04 09:37:28 +1100

    Make cocktail models

--- MixrAi/mixr/admin.py
+++ MixrAi/mixr/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Cocktail, Tag, Ingredient, CocktailIngredient
+
+@admin.register(Cocktail)
+class cocktail_admin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class tag_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Ingredient)
+class ingredient_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(CocktailIngredient)
+class cocktail_ingredient_admin(admin.ModelAdmin):
+    pass
--- MixrAi/mixr/models.py
+++ MixrAi/mixr/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class Cocktail(models.Model):
+    CATEGORY_CHOICES = [
+        ('Ordinary Drink', 'Ordinary Drink'),
+        ('Cocktail', 'Cocktail'),
+        ('Shake', 'Shake'),
+        ('Other / Unknown', 'Other / Unknown'),
+        ('Cocoa', 'Cocoa'),
+        ('Shot', 'Shot'),
+        ('Coffee / Tea', 'Coffee / Tea'),
+        ('Homemade Liqueur', 'Homemade Liqueur'),
+        ('Punch / Party Drink', 'Punch / Party Drink'),
+        ('Beer', 'Beer'),
+        ('Soft Drink', 'Soft Drink'),
+    ]
+
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    IBA = models.CharField(max_length=100, null=True, blank=True)
+    alcoholic = models.BooleanField(default=True)
+    glass = models.CharField(max_length=100)
+    instructions = models.TextField()
+    thumbnail_image = models.URLField()
+
+    tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient', through='CocktailIngredient')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """Tags describing attributes of a Cocktail"""
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class CocktailIngredient(models.Model):
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    measurement = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.cocktail.name + "<-" + self.ingredient.name
